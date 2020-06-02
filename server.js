37a6f0e4022318f5dfeb64fc5a39ba90f0849a29
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-02 15:34:32 -0400

    fixed mongodb url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/budget";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const options = {
 	useNewUrlParser: true,
